1d94974c0071b4ffffb41a613838c5d94dafd6ed
MariePerraud <m100chez@gmail.com>
2022-05-12 17:22:47 +0200

    création de la roue sur la page d'accueil

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,66 @@
-import Counter from "@components/Counter";
-import logo from "@assets/logo.svg";
+import React, { useState } from "react";
+import { Wheel } from "react-custom-roulette";
+import "../styles/Home.css";
+
+const data = [
+  { id: 1, option: "🐧" },
+  { id: 2, option: "🐊" },
+  { id: 3, option: "🐝" },
+  { id: 4, option: "🦍" },
+  { id: 5, option: "🐯" },
+  { id: 6, option: "🐳" },
+  { id: 7, option: "🐼" },
+  { id: 8, option: "🐺" },
+  { id: 9, option: "🦭" },
+  { id: 10, option: "🐢" },
+];
 
 export default function Home() {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  const handleSpinClick = () => {
+    const newPrizeNumber = Math.floor(Math.random() * data.length);
+    setPrizeNumber(newPrizeNumber);
+    setMustSpin(true);
+  };
+
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <div>
+      <div align="center">
+        <h1 align="center">La Roue de la Malchance</h1>
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={data}
+          outerBorderColor={["#E24A2B"]}
+          outerBorderWidth={[5]}
+          innerBorderColor={["#f2f2f2"]}
+          radiusLineColor={["#dedede"]}
+          radiusLineWidth={[2]}
+          textColors={["#ffffff"]}
+          fontSize={[45]}
+          perpendicularText={[true]}
+          backgroundColors={[
+            "#7353BA",
+            "#FFC759",
+            "#3C91E6",
+            "#FF7B9C",
+            "#06D6A0",
+          ]}
+          onStopSpinning={() => {
+            setMustSpin(false);
+          }}
+        />
+        <button type="button" className="button2" onClick={handleSpinClick}>
+          Lancer <br />
+          la Roue
+        </button>
+        <br />
+        <div className="RandomResponse">
+          {!mustSpin ? data[prizeNumber].option : ""}
+        </div>
+      </div>
+    </div>
   );
 }
